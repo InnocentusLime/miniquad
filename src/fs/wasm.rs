cbbbfb1fb55b0d6736dbaf4e4840e36e9062f7dd
InnocentusLime <innocentuslime.help@gmail.com>
2026-03-30 12:26:11 +0300

    fs(wasm): make the fs server impl more robust
    Make the fs server throw proper errors instead of spamming expect().

--- src/fs/wasm.rs
+++ src/fs/wasm.rs
@@ -2,37 +2,32 @@ use crate::AppEvent;
 
 use super::{FileReady, FsServer};
 
+use std::ffi::OsStr;
 use std::io;
 use std::path::{Path, PathBuf};
 use std::rc::Rc;
-use std::str::FromStr;
 
 use wasm_bindgen::prelude::*;
-use web_sys::Response;
 use web_sys::js_sys::{JSON, Uint8Array};
+use web_sys::{Response, Window};
 use winit::event_loop::EventLoopProxy;
 
 pub(crate) fn spawn_fs_server(
     event_loop_proxy: EventLoopProxy<AppEvent>,
     fs_root: PathBuf,
 ) -> Rc<dyn FsServer> {
-    let window = web_sys::window().expect("\"window\" not found");
-    let page_url = window
-        .location()
-        .pathname()
-        .expect("Could not get location.pathname");
-    let mut page_url = PathBuf::from_str(&page_url).expect("Page url parse");
-    if let Some(ext) = page_url.extension()
-        && ext == "html"
-    {
-        page_url.pop();
+    let window = web_sys::window().expect("window inaccessible");
+    let page_url_pathname = window.location().pathname().expect("no url pathname");
+    let mut page_url_pathname = Path::new(&page_url_pathname).to_path_buf();
+    if page_url_pathname.extension() == Some(OsStr::new("html")) {
+        page_url_pathname.pop();
     }
-
-    Rc::new(WasmFsServer { fs_root, page_url, event_loop_proxy })
+    Rc::new(WasmFsServer { window, fs_root, page_url_pathname, event_loop_proxy })
 }
 
 struct WasmFsServer {
-    page_url: PathBuf,
+    window: Window,
+    page_url_pathname: PathBuf,
     fs_root: PathBuf,
     event_loop_proxy: EventLoopProxy<AppEvent>,
 }
@@ -40,26 +35,34 @@ struct WasmFsServer {
 impl FsServer for WasmFsServer {
     fn load_file(&self, path: &Path) {
         let path: Rc<Path> = path.into();
-        let url = PathBuf::from_iter([&self.page_url, &self.fs_root, &*path])
+        let url = PathBuf::from_iter([&self.page_url_pathname, &self.fs_root, &*path])
             .to_string_lossy()
             .into_owned();
-        let window = web_sys::window().expect("\"window\" not found");
-        let proxy = self.event_loop_proxy.clone();
-        let then_callback = Closure::new(move |val| fetch_handler(val, path.clone(), &proxy));
 
-        let _ = window.fetch_with_str(&url).then(&then_callback);
-        then_callback.forget();
-    }
-}
+        let resolve_path = path.clone();
+        let resolve_proxy = self.event_loop_proxy.clone();
+        let resolve_callback = Closure::once(move |val| {
+            match fetch_handler_impl(val, resolve_path.clone(), resolve_proxy.clone()) {
+                Ok(()) => Ok(()),
+                Err(e) => send_result(Err(e), resolve_path, &resolve_proxy),
+            }
+        });
 
-fn fetch_handler(val: JsValue, path: Rc<Path>, proxy: &EventLoopProxy<AppEvent>) {
-    if let Err(e) = fetch_handler_impl(val, path.clone(), proxy.clone()) {
-        proxy
-            .send_event(AppEvent::FileReady(FileReady {
-                path: path.to_path_buf(),
-                bytes_result: Err(e),
-            }))
-            .expect("Loop died");
+        let reject_path = path.clone();
+        let reject_proxy = self.event_loop_proxy.clone();
+        let reject_callback = Closure::once(move |val| {
+            // fetch fails only when we couldn't establish a connection or CORS kicks in.
+            let err = io::Error::new(io::ErrorKind::NetworkUnreachable, js_val_to_errmsg(val));
+            send_result(Err(err), reject_path, &reject_proxy)
+        });
+
+        let _ = self
+            .window
+            .fetch_with_str(&url)
+            .then_with_reject(&resolve_callback, &reject_callback);
+        // Handover the closures to JS. They will be GC'd once the request finishes.
+        resolve_callback.forget();
+        reject_callback.forget();
     }
 }
 
@@ -76,42 +79,42 @@ fn fetch_handler_impl(
     let array_buffer = response
         .array_buffer()
         .map_err(|err| js_val_to_io_error("failed to get response body", err))?;
-    let then_closure = Closure::new(move |val| array_buffer_handler(val, path.clone(), &proxy));
-    let _ = array_buffer.then(&then_closure);
-    then_closure.forget();
+
+    let resolve_path = path.clone();
+    let resolve_proxy = proxy.clone();
+    let resolve_callback = Closure::once(move |val| {
+        let bytes = Uint8Array::new(&val).to_vec();
+        send_result(Ok(bytes), resolve_path, &resolve_proxy)
+    });
+
+    let reject_path = path.clone();
+    let reject_proxy = proxy.clone();
+    let reject_callback = Closure::once(move |val| {
+        // array_buffer fails only when the connection cuts off.
+        let err = io::Error::new(io::ErrorKind::ConnectionAborted, js_val_to_errmsg(val));
+        send_result(Err(err), reject_path, &reject_proxy)
+    });
+
+    let _ = array_buffer.then_with_reject(&resolve_callback, &reject_callback);
+    // Handover the closures to JS. They will be GC'd once the request finishes.
+    resolve_callback.forget();
+    reject_callback.forget();
     Ok(())
 }
 
-fn array_buffer_handler(val: JsValue, path: Rc<Path>, proxy: &EventLoopProxy<AppEvent>) {
-    let bytes = Uint8Array::new(&val).to_vec();
-    proxy
-        .send_event(AppEvent::FileReady(FileReady {
-            path: path.to_path_buf(),
-            bytes_result: Ok(bytes),
-        }))
-        .expect("Loop died");
-}
-
 fn check_http_status(response: &Response) -> io::Result<()> {
+    let mk_err_simpl = |kind| Err(io::Error::new(kind, response.status_text()));
     match response.status() {
+        // Mimiq expects assets to be hosted on a simple static server.
+        // Only the 200 status makes sense for us.
+        // Everything else singals a complicated API we aren't designed for.
         200 => Ok(()),
-        404 | 410 => Err(io::Error::new(
-            io::ErrorKind::NotFound,
-            response.status_text(),
-        )),
-        400 | 405 | 406 | 411 | 412 | 413 | 414 | 415 | 416 | 417 | 421 | 422 | 423 | 424 | 425
-        | 426 | 428 | 431 => Err(io::Error::new(
-            io::ErrorKind::InvalidInput,
-            response.status_text(),
-        )),
-        401 | 402 | 403 | 407 | 451 | 511 => Err(io::Error::new(
-            io::ErrorKind::PermissionDenied,
-            response.status_text(),
-        )),
-        500..511 | 512..600 => Err(io::Error::new(
-            io::ErrorKind::NetworkDown,
-            response.status_text(),
-        )),
+        404 | 410 => mk_err_simpl(io::ErrorKind::NotFound),
+        400 | 405 | 406 | 411..=417 | 421..=426 | 428 | 431 => {
+            mk_err_simpl(io::ErrorKind::InvalidInput)
+        }
+        401 | 402 | 403 | 407 | 451 | 511 => mk_err_simpl(io::ErrorKind::PermissionDenied),
+        500..=510 | 512..=599 => mk_err_simpl(io::ErrorKind::NetworkDown),
         unexpected => Err(io::Error::new(
             io::ErrorKind::Other,
             format!(
@@ -120,6 +123,21 @@ fn check_http_status(response: &Response) -> io::Result<()> {
             ),
         )),
     }
+}
+
+fn send_result(
+    bytes_result: io::Result<Vec<u8>>,
+    path: Rc<Path>,
+    proxy: &EventLoopProxy<AppEvent>,
+) -> Result<(), JsError> {
+    // Let event loop closing bubble up as an unhandled promise error.
+    // This way the error cleanly shows up in the console without creating a Rust panic.
+    proxy
+        .send_event(AppEvent::FileReady(FileReady {
+            path: path.to_path_buf(),
+            bytes_result,
+        }))
+        .map_err(JsError::from)
 }
 
 fn js_val_to_io_error(preamble: &str, err: JsValue) -> io::Error {
