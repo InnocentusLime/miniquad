a904e33fe02c6e8146503cada01d30f7a58edebb
InnocentusLime <innocentuslime.help@gmail.com>
2026-05-06 20:34:35 +0300

    audio: simplify the converter

--- src/audio/conv.rs
+++ src/audio/conv.rs
@@ -10,23 +10,24 @@ pub fn normalize_audio<I>(samples: I, spec: hound::WavSpec) -> Result<Vec<I16Vec
 where
     I: Iterator<Item = hound::Result<i16>>,
 {
+    let sample_scaler: fn(i16) -> i16 = match spec.bits_per_sample {
+        8 => |x| x.saturating_mul(i8::MAX as i16),
+        16 => |x| x,
+        found => return Err(Error::UnexpectedBitdepth { found }),
+    };
+
     let scaled = samples
         .map(|x| x.map_err(Error::WavParsingError))
-        .map(|x| scale_samples(x, spec));
-    ChannelIter { spec, iter: scaled }.collect()
-}
+        .map(|x| x.map(sample_scaler));
 
-fn scale_samples(x: Result<i16>, spec: hound::WavSpec) -> Result<i16> {
-    let x = x?;
-    match spec.bits_per_sample {
-        8 => Ok(x.saturating_mul(i8::MAX as i16)),
-        16 => Ok(x),
-        found => Err(Error::UnexpectedBitdepth { found }),
+    match spec.channels {
+        1 => scaled.map(|x| x.map(I16Vec2::splat)).collect(),
+        2 => ChannelIter { iter: scaled }.collect(),
+        found => Err(Error::UnexpectedChannelCount { found }),
     }
 }
 
 struct ChannelIter<I> {
-    spec: hound::WavSpec,
     iter: I,
 }
 
@@ -36,38 +37,20 @@ impl<I: Iterator<Item = Result<i16>>> Iterator for ChannelIter<I> {
     type Item = Result<I16Vec2>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        match self.spec.channels {
-            1 => next_mono(&mut self.iter),
-            2 => next_stereo(&mut self.iter),
-            found => Some(Err(Error::UnexpectedChannelCount { found })),
+        let l: i16;
+        match self.iter.next() {
+            None => return None,
+            Some(Err(e)) => return Some(Err(e)),
+            Some(Ok(x)) => l = x,
         }
+
+        let r: i16;
+        match self.iter.next() {
+            None => return Some(Err(Error::UnexpectedWAVEOF)),
+            Some(Err(e)) => return Some(Err(e)),
+            Some(Ok(x)) => r = x,
+        }
+
+        Some(Ok(i16vec2(l, r)))
     }
-}
-
-fn next_mono<I>(iter: &mut I) -> Option<Result<I16Vec2>>
-where
-    I: Iterator<Item = Result<i16>>,
-{
-    iter.next().map(|x| x.map(I16Vec2::splat))
-}
-
-fn next_stereo<I>(iter: &mut I) -> Option<Result<I16Vec2>>
-where
-    I: Iterator<Item = Result<i16>>,
-{
-    let l: i16;
-    match iter.next() {
-        None => return None,
-        Some(Err(e)) => return Some(Err(e)),
-        Some(Ok(x)) => l = x,
-    }
-
-    let r: i16;
-    match iter.next() {
-        None => return Some(Err(Error::UnexpectedWAVEOF)),
-        Some(Err(e)) => return Some(Err(e)),
-        Some(Ok(x)) => r = x,
-    }
-
-    Some(Ok(i16vec2(l, r)))
 }
